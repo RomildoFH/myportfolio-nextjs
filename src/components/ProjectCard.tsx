8c75527b1ea56c922651cf236bd646b457c54f63
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-10-06 19:07:08 -0300

    inserida a seção de projetos

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './ProjectCard.module.css';
+import Image from 'next/image';
 
-function ProjectCard() {
+function ProjectCard(props: { name: string; thumbs: string; description: string; url: string; }) {
+  const { name, thumbs, description, url } = props;
   return (
     <article className={styles['project-card']}>
-      
+      <img
+        className={styles['project-image']}
+        src={thumbs}
+        alt={name}
+      />
+      <div className={styles['project-data']}>
+        <h3 className={styles['project-title']}>{name}</h3>
+        <p className={styles['project-description']}>{description}</p>
+        <a href={url} target='_blank' className={styles['project-link']}>Abrir Projeto</a>
+      </div>
     </article>
   )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import styles from './Projects.module.css';
+import projects from '../utils/projects';
+import ProjectCard from './ProjectCard';
 
 function Projects() {
   return (
-    <section className={styles['projects-container']}>Projects</section>
+    <section className={styles['projects-container']}>
+      <h2>Meus projetos</h2>
+      {
+        projects.map((project) => (
+          <ProjectCard
+            key={project.name}
+            name={project.name}
+            thumbs={project.thumbs}
+            description={project.description}
+            url={project.url}            
+          />
+        ))
+      }
+    </section>
   )
 }
 
