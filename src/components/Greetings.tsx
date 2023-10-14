8bc41a9ab059a729000f70ee1154c57aa5bf811e
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-10-14 11:44:15 -0300

    feat: implementada animação de typing no título

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import style from './Greetings.module.css';
 import Image from 'next/image'
 import scroll from '../images/scroll-bar.png';
 
-function Greetings (){
+function Greetings() {
+
+  const [phrase, setPhrase] = useState('first');
+  const [currentPhrase, setCurrentPhrase] = useState('');
+  const [action, setAction] = useState('write');
+
+  const firstPhrase = 'Olá, meu nome é Romildo    ';
+  const secoundPhrase = 'Eu sou um desenvolvedor web full-stack    ';
+
+  const writePhrase = (letters: string, phrase: string) => {
+    const index = letters.length;
+    letters += phrase[index]
+    setTimeout(() => {
+      setCurrentPhrase(letters);
+    }, 100);
+  };
+
+  const erasePhrase = () => {
+    setTimeout(() => {
+      const newPhrase = currentPhrase.slice(0, -1);
+      setCurrentPhrase(newPhrase);
+    }, 75);
+  };
+
+  useEffect(() => {
+    const newFirstPhrase = firstPhrase + '    ';
+    const newScoundPhrase = secoundPhrase + '    ';
+    let letters = currentPhrase;
+    if (phrase === 'first' && action === 'write' && currentPhrase !== newFirstPhrase) {
+      writePhrase(letters, newFirstPhrase);
+    }
+    if (letters === newFirstPhrase || letters === newScoundPhrase) {
+      setAction('erase');
+    }
+
+    if (action === 'erase' && currentPhrase.length > 0){
+      erasePhrase();
+    }
+
+    if (action === 'erase' && currentPhrase.length === 0) {
+      setAction('write');
+      setPhrase('secound');
+    }
+
+    if (phrase === 'secound' && action === 'write' && currentPhrase !== newScoundPhrase) {
+      writePhrase(letters, newScoundPhrase);
+    }
+  }, [currentPhrase, action]);
+
+
   return (
     <section className={style['greetings']}>
-      <h3 className={style.title}>Meu nome é Romildo</h3>
+      <h3 className={style.title}>{currentPhrase}</h3>
       <p className={style.paragraph}>Este é o meu portfolio personalizado, onde você vai poder conhecer um pouco sobre mim e meu trabalho. </p>
       <Image
         src={scroll}
